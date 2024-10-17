6df0be5463e7479cce67347fa273b5b2de23b748
lynx <lynx@kali.kali222>
2024-10-17 16:52:15 -0700

    Lab #7 updated

--- jaye7.c
+++ jaye7.c
@@ -1,8 +1,9 @@
-
 //John Aye
 //Sync example
+
 #include <stdio.h>
 #include <stdlib.h>
+
 //this is a function prototype that lets the main know a function is available at the end of the program.
 int compare_and_swap(int *pointb, int expected, int new_value);
 
@@ -10,55 +11,66 @@ void swap_values(int *value1, int *value2, int *value3, int *value4);
 
 int main(void)
 {
-/*using pointers is helpful when you want to permanantely set the value of a variable
- * using functions. Normally when variables are created or modified in functions they disapear
-* after the function completes, with pointers you are changing a value by using it's address.
-*/
-	int *pointa;
-	int expected = 1;
-	int newValue = 22;
-	int var1 = 10;
-	int var2 = 20;
-
-//A pointer only points to the address space of another value.
+    	/*using pointers is helpful when you want to permanantely set the value of a
+	variable
+	* using functions. Normally when variables are created or modified in
+	functions they disapear
+	* after the function completes, with pointers you are changing a value by
+	using it's address.
+	*/
+    int *pointa;
+    int expected = 1;
+    int newValue = 22;
+    int var1 = 10;
+    int var2 = 20;
+    
+	//A pointer only points to the address space of another value.
 	pointa = &expected;
-
-//pointa now points to the address space of the value expected.
-//to see the address stored in the pointer we print out the pointer normally
-//as it only stores addresses.
+	//pointa now points to the address space of the value expected.
+	//to see the address stored in the pointer we print out the pointer normally
+	//as it only stores addresses.
 	printf(" address stored in pointa is %p \n",pointa);
-//if we want to see if that address is the same as the variable expected
+	//if we want to see if that address is the same as the variable expected
 	printf(" address of variable named expected is %p \n",&expected);
-//these print statment print the same thing because we set our pointer to point to expected
-//If we want to see the value that the pointer is pointing to,
-//we use the * before our pointer
+	//these print statment print the same thing because we set our pointer to point to expected
+	//If we want to see the value that the pointer is pointing to,
+	//we use the * before our pointer
 	printf(" value that pointa points to is %d \n",*pointa);
-//this should be the the same as the value of expected
+	//this should be the the same as the value of expected
 	printf(" value of expected is %d \n",expected);
-//when we call a separate function we may need to pass it variables
-//this function requires three variable to be passed to it
-//it also returns a value to we need to save that value after the function completes
-	int valueReturned = compare_and_swap(pointa,expected,newValue);
-//after the function completes it should have swapped expected with new value
-	printf(" expected = %d, old value which was returned is %d\n", expected, valueReturned);
+	//when we call a separate function we may need to pass it variables
+	//this function requires three variable to be passed to it
+	//it also returns a value to we need to save that value after the function completes
 	
-	return EXIT_SUCCESS;
+    int valueReturned = compare_and_swap(pointa, expected, newValue);
+    //after the function completes it should have swapped expected with new value
+    printf("expected = %d, old value which was returned is %d\n", expected, valueReturned);
+
+    //display var1 and var2 before swapping
+    printf("Before swapping: var1 = %d, var2 = %d\n", var1, var2);
+    
+    swap_values(&valueReturned, &expected, &var1, &var2);
+
+    printf("After swapping: expected = %d, valueReturned = %d\n", expected, valueReturned);
+    printf("After swapping: var1 = %d, var2 = %d\n", var1, var2);
+
+    return EXIT_SUCCESS;
 }
 
-	int compare_and_swap(int *pointb, int expected, int new_value)
+int compare_and_swap(int *pointb, int expected, int new_value)
+{
+	//we can use the same name for expected and it will be a different variable in this function.
+	int temp = *pointb;
+	//created a temp variable to store the original value located at the pointer pointb
+	if(*pointb == expected)
 	{
-//we can use the same name for expected and it will be a different variable in this function.
-		int temp = *pointb;
-//created a temp variable to store the original value located at the pointer point
-		if(*pointb == expected)
-		{
 		*pointb = new_value;
-		}
-		return temp;
 	}
+	return temp;
+}
 
-	void swap_values(int *value1, int *value2, int *value3, int *value4)
-	{
+void swap_values(int *value1, int *value2, int *value3, int *value4)
+{
 	int temp = *value1;
 	*value1 = *value2;
 	*value2 = temp;
@@ -66,4 +78,5 @@ int main(void)
 	temp = *value3;
 	*value3 = *value4;
 	*value4 = temp;
-	}
+}
+
